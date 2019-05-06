581e1429bd52dd1847381ae494484158f19c6cf3
Arnar Snaer <arnarsnaer98@gmail.com>
2019-05-06 14:57:57 +0000

    Initial commit

---
+++ Castle_apartments_base/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\arnar\Documents\Other\Háskóli\Önn 2\3ja vikna áfangi\Castle_apartments_base\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
